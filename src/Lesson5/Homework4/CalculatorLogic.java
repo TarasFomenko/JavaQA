6dbbd230e5279e27b61bbeae72391ac3188992ed
Taras Fomenko <fomenkotarasevg@gmail.com>
2023-04-07 00:43:11 +0200

    Adding Homework 4

--- src/Lesson5/Homework4/CalculatorLogic.java
+++ src/Lesson5/Homework4/CalculatorLogic.java
@@ -1,25 +1,25 @@
 package Lesson5.Homework4;
 
-public class CalculatorLogic {
+class CalculatorLogic {
 
-    public static int add(int number1, int number2) {
+    static int add(int number1, int number2) {
         return number1 + number2;
     }
 
-    public static int subtract(int number1, int number2) {
+    static int subtract(int number1, int number2) {
         return number1 - number2;
     }
 
-    public static int multiply(int number1, int number2) {
+    static int multiply(int number1, int number2) {
         return number1 * number2;
     }
 
 
-    public static double divide(int number1, int number2) {
+    static double divide(int number1, int number2) {
         return (double) number1 / number2;
     }
 
-    public static double modulo (int number1, int number2){
+    static double modulo (int number1, int number2){
         return (double) number1 % number2;
     }
 
