c03a8f3cfdd93c331f2115607edc7fa139523c5e
Taras Fomenko <fomenkotarasevg@gmail.com>
2023-04-21 23:24:38 +0200

    Lesson 7 - Homework 6

---
+++ src/Lesson8/Animal.java
@@ -0,0 +1,12 @@
+package Lesson8;
+
+public abstract class Animal {
+    private String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    abstract void greets();
+}
---
+++ src/Lesson8/Pet.java
@@ -0,0 +1,7 @@
+package Lesson8;
+
+public interface Pet {
+     void feed();
+     void play();
+     void walk();
+}
