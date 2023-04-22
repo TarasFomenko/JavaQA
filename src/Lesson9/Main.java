afc8345e5344a8915de09f531c401e577a415fc2
Taras Fomenko <fomenkotarasevg@gmail.com>
2023-04-22 02:32:01 +0200

    Lesson 9 - Homework 9

---
+++ src/Lesson9/Main.java
@@ -0,0 +1,10 @@
+package Lesson9;
+
+public class Main {
+    public static void main(String[] args) {
+        Size.Small.toString();
+        Size.Medium.toString();
+        Size.Large.toString();
+        Size.ExtraLarge.toString();
+    }
+}
