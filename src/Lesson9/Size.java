16ef8489a0a42c257d04a7edd54a5443885cf955
Taras Fomenko <fomenkotarasevg@gmail.com>
2023-04-22 02:31:38 +0200

    Lesson 9 - Homework 9

--- src/Lesson9/Size.java
+++ src/Lesson9/Size.java
@@ -1,31 +1,36 @@
 package Lesson9;
 
 public enum Size {
-    Small, Medium, Large, ExtraLarge;
+    Small("S", 11, 41),
+    Medium ("M", 12, 42),
+    Large ("L", 13, 43),
+    ExtraLarge ("XL", 14, 44);
 
-    String shortName;
-    double width;
-    double length;
+    private final String shortName;
+    private final int width;
+    private final int length;
 
     Size(String shortName, int width, int length) {
         this.shortName = shortName;
         this.width = width;
         this.length = length;
     }
+
     public String getShortName() {
         return shortName;
     }
 
-    public double getWidth() {
+    public int getWidth() {
         return width;
     }
 
-    public double getLength() {
+    public int getLength() {
         return length;
     }
 
     @Override
     public String toString() {
-        return "Size{}";
+        System.out.println(getShortName() + " size, with width = " + getWidth() +  " and length = " + getLength());
+        return null;
     }
 }
